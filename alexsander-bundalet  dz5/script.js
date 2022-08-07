11c6ea0fea5d46280531babdec47812c90351ad8
Your Name <uso091217752321993@gmail.com>
2022-08-07 14:22:00 +0300

    dz5

---
+++ alexsander-bundalet  dz5/script.js
@@ -0,0 +1,154 @@
+// prompt('sadjisadi')
+
+
+// let textInp = prompt('text input')
+// arrayFill(textInp)
+// function arrayFill (y,lenght){
+//     let arr = [];
+//     for (let i = 0; i < lenght; i++){
+//         arr. push (y);
+//     } 
+//     return arr;
+
+// }
+// console.log (arrayFill('x',textInp));
+
+
+// let textInp = prompt('text input')
+// matrixArray(textInp)
+
+
+
+
+
+
+
+// let number = prompt('text input')
+// isNumberInRange(number)
+
+// function isNumberInRange (num){
+//    return num > 0 && num < 10;
+// }
+// console.log(isNumberInRange(number))
+
+
+
+
+
+// let number = prompt('text input')
+// isEven(number)
+// function isEven(even){
+// return even % 2 == 0 ;
+// }
+// console.log(isEven(number))
+
+
+
+
+
+// let arr = [1,2,3,4,5,6,7];
+// let newArr = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (isEven(i)) {
+//     newArr.push(i);
+//   }
+// }
+
+// console.log(newArr);
+
+// function isEven(num) {
+//   return num % 2 == 0;
+// }
+
+
+// let number = prompt('text input')
+// muFun(number)
+// let number2 = prompt('text input')
+// muFun(number2)
+// function muFun (number,number2){
+// return number**number2
+
+
+// }
+// console.log(muFun(number,number2))
+
+
+
+// let number = prompt('text input')
+// muFun(number)
+// let a  = number
+// function muFun (a){
+//     let a;
+//     if(a>16)console.log(muFun('добро пожаловать'));
+//     if(a<16)console.log(muFun('недобро пожаловать'));
+    
+    
+//     else if (a >= 0 && a < 16);
+    
+   
+//     else if (age === undefined ||  age < 0 || isNaN(a))
+    
+
+// return muFun (3);
+
+ 
+// }
+
+
+
+
+
+
+    // let str = 'You should read';
+    // let res = [];
+    
+    // let arr = str.split(' ');
+    // for (let i = 0; i < arr.length; i++) {
+    //   res.push(ucfirst(arr[i]));
+    // }
+    
+    // let newStr = res.join(' ');
+    
+    // console.log(res);
+    // console.log(newStr);
+    
+    // function ucfirst(word) {
+    //   return word[0].toUpperCase() + word.substr(1);
+    // }
+
+
+
+
+//    let WorkString = 'var_text_hello';
+//                 function first(str) {
+//                     let firstLetter = str.substr(0,1);
+//                     return firstLetter + str.substr(1);
+//                 }
+//                 function sampl(usrString) {
+//                     let workArray = usrString.split('_');
+//                     for (let i = 0; i < workArray.length; i++) {
+//                         workArray[i] = first(workArray[i]);
+//                     }
+//                     console.log(workArray.join(''));
+//                 }
+
+//                 console.log( WorkString  );
+//                 sampl(WorkString);
+
+
+
+// function inArray(value,arr) {
+//       for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] == value) {
+//           return true;
+//         }
+//       }
+//       return false;
+//     }
+    
+//     var mas = [54,6,99,,6,5,7,8,94,,4,100,6,7];
+    
+//     console.log(inArray(7,mas));
+
+ 
